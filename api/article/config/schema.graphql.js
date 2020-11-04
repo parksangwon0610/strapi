f91482648608d677cef0f0c4d519e81bc91627c8
swpark <swpark@smartauth.kr>
2020-11-04 14:22:11 +0900

    수정 / 2일차 과제 끝

--- api/article/config/schema.graphql.js
+++ api/article/config/schema.graphql.js
@@ -1,10 +1,14 @@
 module.exports = {
   definition: `
+    input ArticleIDInput {
+      id: String
+    }
   `,
   query: `
     articleBySlug(slug:String): [Article]
   `,
   mutation: `
+    addMyComment(content: String, article: ArticleIDInput ): Comment
   `,
   type: {
   },
@@ -16,6 +20,10 @@ module.exports = {
       }
     },
     Mutation: {
+      addMyComment: {
+        description: 'content 필드값과 현재 선택된 article 번호를 인수로 받아 request header에 포함된 user가 자동으로 writer로 저장되는 기능',
+        resolver: 'application::article.article.addMyComment',
+      }
     }
   },
 };
--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,12 +1,32 @@
 'use strict';
 const slugify = require('slugify');
 module.exports = {
+
   findBySlug: async function(ctx) {
     console.log('findBySlug ctx.query >>> ', ctx.query);
     return await strapi.services.article.find(ctx.query);
   },
+
   articleBySlug: async function(ctx) {
     console.log('articleBySlug ctx.query >>> ', ctx.query);
     return await strapi.services.article.find({slug:ctx.query._slug});
+  },
+
+  addMyComment: async function(ctx) {
+    console.log('addMyComment data >>> ', ctx.request.body);
+    console.log('addMyComment jwt  >>> ', ctx.header.authorization);
+    console.log('addMyComment user  >>> ', ctx.state.user);
+
+    const requestQueryData = ctx.request.body;
+    const foundMember = await strapi.services.member.findOne({user: ctx.state.user.id});
+
+    const createdComment = await strapi.services.comment.create({
+      content: requestQueryData.content,
+      article: requestQueryData.article.id,
+      writer: foundMember.id
+    });
+    console.log('created Comment >>> ', createdComment);
+    return createdComment;
   }
+
 };
